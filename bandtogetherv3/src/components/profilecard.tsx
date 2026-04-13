d24978a0ef12c75c05cd8e855da5c86dfe6c93df
Mattiza58 <matthew58izaguirre@gmail.com>
2026-04-13 16:55:47 -0400

    More profile changes

--- bandtogetherv3/src/components/profilecard.tsx
+++ bandtogetherv3/src/components/profilecard.tsx
@@ -3,43 +3,49 @@ import styles from "./maintext.module.css"
 interface UserProps{
     name: string;
     user_name: string;
+    followers: number;
+    following: number;
+    location: string;
+    instruments: string;
+    genres: string;
+    yrs_experience: string
 } 
 
-const ProfileCard = ({name, user_name}: UserProps) =>{
+const ProfileCard = ({name, user_name, followers, following, location, instruments, genres, yrs_experience}: UserProps) =>{
     return <div className={styles.profile_card_container}>
         <div className={styles.card_container}>
-            <div className={styles.profile_image}>
-            </div>
+            <img src = "/placeholder_icon.png" className={styles.profile_image}>
+            </img>
             <div style = {{fontFamily: "Be Vietnam Pro, sans-serif",  
                 padding: "10px", fontSize: "1.25rem", margin: "10px"}}>
                 {name}<br></br>
-                <div style = {{fontFamily: "Be Vietnam Pro, sans-serif", paddingTop: "5px", fontSize: "1rem"}}>
+                <div style = {{paddingTop: "5px", fontSize: "1rem"}}>
                     {user_name}
+                </div>
+                <div style = {{paddingTop: "10px", fontSize: "0.9rem"}}>
+                <b>{followers}</b> Followers <b>{following}</b> Following
                 </div>
             </div>
             <button className = {styles.edit_profile_button}>
                 Edit Profile
             </button>
-            <ul>
+            <ul style = {{fontFamily: "Be Vietnam Pro, sans-serif"}}>
                 <li>
-                    <div style = {{gap: "10px"}}>
-                        <img src ="/location_icon.png" style = {{display: "inline", width: "15px"}}></img>
-                        hello
-                    </div>
+                    {location}
                 </li>
                 <li>
-                    hi
+                    {instruments}
                 </li>
                 <li>
-                    hi
+                    {genres}
                 </li>
                 <li>
-                    hi
+                    {yrs_experience}
                 </li>
             </ul>
         </div>
         <div className={styles.card_container}>
-            hi
+            Upcoming Shows
         </div>
     </div>
 }
--- bandtogetherv3/src/pages/profile.tsx
+++ bandtogetherv3/src/pages/profile.tsx
@@ -4,7 +4,7 @@ import ProfileCard from "../components/profilecard";
 const Profile = (() =>{
     return <div>
         <LargeText link = "Profile"/>
-        <ProfileCard name = "Matthew Izaguirre" user_name="@mattiza58"/>
+        <ProfileCard name = "Matthew Izaguirre" user_name="@mattiza58" followers= {375} following={371} location="Ithaca, NY" instruments="Guitar, Bass" genres="Rock, Metal, Indie" yrs_experience="4+ Years Experience" />
     </div>
 })
 
